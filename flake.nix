174fdd20edc4f504e012cedc7798ee7c9847f142
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2026-08-11 03:52:21 +0900

    Add Rust Nix flake
    * nix: add Rust development flake
    
    * ci: validate Nix Rust shell
    
    * nix: lock development inputs
    
    * ci: keep Nix shell validation
    
    * docs: document Nix Rust environment

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Sashimi Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, rust-overlay, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system:
+          f (import nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
+          }));
+    in
+    {
+      devShells = forAllSystems (pkgs:
+        let
+          rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+            extensions = [
+              "clippy"
+              "rust-analyzer"
+              "rust-src"
+              "rustfmt"
+            ];
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [ rustToolchain ];
+
+            RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+          };
+        });
+
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-tree);
+    };
+}
